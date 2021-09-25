58a3330ed9ebaf5d7b6f1672c2002de747e82b4d
keita811627 <pan.811627@gmail.com>
2021-09-25 07:32:08 +0000

    bookers

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+
+  resources :books
+  get 'top' => 'homes#top'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  root to: 'homes#top'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
